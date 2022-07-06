bf2b4df62032553ef3ce8ced35705562c9205eee
Mauro Gutierrez <yoguiimaster@gmail.com>
2022-07-06 19:04:25 -0300

    feat: products services and controller actualized

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -6,85 +6,40 @@ import {
   HttpCode,
   HttpStatus,
   Param,
-  ParseIntPipe,
-  Patch,
   Post,
   Put,
-  Query,
-  Res,
 } from '@nestjs/common';
-import { Response } from 'express';
 import { Product } from './interfaces/product.interface';
 import { ProductsService } from './products.service';
 
-interface IUser {
-  name: string;
-  description: string;
-}
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Get() //este es un decorador
+  @Get()
   getAllProducts(): Product[] {
-    return this.productsService.getAll(); //el return esta haciendo el response
-  }
-
-  @Get('hot') //esto hace lo que haciamos antes : "/products/hot"
-  getSpecialProducts(): string {
-    return 'Te vamos a mostrar los productos más calientes!!';
-  }
-
-  // @Get(':id') //Con @Get(':id') estamos indicando que esta ruta tendrá un parámetro llamado "id"
-  // find(@Param('id') id: string) {
-  //   return `Estás consultando el producto ${id}`;
-  // }
-
-  @Get(':id/:size')
-  findWithSize(@Param('id') id: string, @Param('size') size: string) {
-    return `En esta ruta obtenemos el producto ${id}, pero en su tamaño ${size}`;
-  }
-
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  createProduct(@Body() body: IUser) {
-    return `Creo un producto ${body.name} con descripción ${body.description}`;
-  }
-
-  @Get('query')
-  rutaQuery(@Query() query) {
-    return query;
-  }
-
-  @Get('cars')
-  carsQuery(@Query('count', ParseIntPipe) carCount: number) {
-    // el pipe me sirve como algo que verifica si cumple o no la regla para poder pasar ej : no podes pasar porq no tenes la altura suficiente
-    return carCount;
+    return this.productsService.getAll();
   }
 
   @Get(':id')
-  findOne(@Res() response: Response, @Param('id') id: number) {
-    if (id < 100) {
-      return response.status(HttpStatus.OK).send(`Página del producto ${id}`);
-    } else {
-      return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
-    }
+  find(@Param('id') id: number) {
+    return this.productsService.getId(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  createProduct(@Body() body) {
+    this.productsService.insert(body);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() body: IUser) {
-    return `Estás haciendo una operación de actualización del recurso ${id} 
-          con ${body.name} y ${body.description}`;
-  }
-
-  @Patch(':id')
-  partialUpdate(@Param('id') id: number) {
-    return `Actualización parcial del ítem ${id}`;
+  update(@Param('id') id: number, @Body() body) {
+    return this.productsService.update(id, body);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id') id: number) {
-    return `Hemos borrado el producto ${id}`;
+    this.productsService.delete(id);
   }
 }
